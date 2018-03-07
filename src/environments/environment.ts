67476ef5166943313198f3247183a4c9ae1f09eb
Patrick M. Legislador <33364246+3kdono@users.noreply.github.com>
2018-03-07 14:57:25 +0800

    Initial commit. Added route and modified UI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyCZz05vTC29U1SqbxW1joyfPNQGSVzeWGc",
+        authDomain: "angular-social-f450e.firebaseapp.com",
+        databaseURL: "https://angular-social-f450e.firebaseio.com",
+        projectId: "angular-social-f450e",
+        storageBucket: "angular-social-f450e.appspot.com",
+        messagingSenderId: "255888689446"
     }
 };
